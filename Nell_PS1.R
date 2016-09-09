71f627c378621641e134e32b2e6a6998f383d1b5
lucasnell <lucnell@gmail.com>
2016-09-09 09:41:53 -0500

    Switched from scatter to tile plot

--- Nell_PS1.R
+++ Nell_PS1.R
@@ -31,7 +31,7 @@ dat <- read.csv(file = "grouse_data_7Sep16.csv", header = TRUE) %>%
     mutate(DATE = as.Date(DATE, format = "%d-%b")) %>%
     mutate_each(funs(as.factor), STATION, PERIOD)
 
-# dplyr::as.tbl makes printing data frames easier (many cols is still a problem)
+# dplyr::as.tbl makes printing data frames less of a hazard
 dat
 
 
@@ -40,32 +40,37 @@ dat
 # Visualizing entire dataset
 # =========================
 
-# "Tidy" data for easier plotting
+# "Tidy" data for easier plotting & analyzing
 grouse_df <- dat %>%
     gather(species, detected, RUGR, WITU, STGR) %>%
     mutate(
         species = factor(species, levels = c('STGR', 'RUGR', 'WITU'), 
                          labels = c('Sharp-tailed Grouse', 'Ruffed Grouse', 
-                                    'Wild Turkey')))
+                                    'Wild Turkey'))
+    )
 
 # ggplot objects relevant to all plots of spatial distributions of detections
-base_plot <- grouse_df %>%
-    ggplot(aes(x = X_NAD83, y = Y_NAD83, color = as.factor(detected))) +
-    geom_point(shape = 1, size = 2, alpha = 0.5) + 
+base_tile <- {
+    # Calculating proportion of visits at each location that detected grouse on the fly
+    grouse_df %>%
+        group_by(X_NAD83, Y_NAD83, species, PERIOD) %>%
+        summarize(detected = mean(detected)) %>%
+        ungroup %>%
+        select(species, PERIOD, X_NAD83, Y_NAD83, detected)
+    } %>%
+    # Now for the plot itself
+    ggplot(aes(x = X_NAD83, y = Y_NAD83, fill = detected)) +
+    geom_tile(aes(width = 1e3, height = 1e3)) +
     # Asthetics...
-    scale_color_manual(values = c('tan1', 'black')) +
     theme_bw() + 
-    theme(legend.position = 'none', axis.text = element_blank(), 
-          axis.ticks = element_blank()) + 
+    scale_fill_gradient(low = "gray90", high = "black", guide = "colorbar") +
+    theme(axis.text = element_blank(), axis.ticks = element_blank(),
+          panel.grid = element_blank()) + 
     scale_x_continuous("Longitude") + 
     scale_y_continuous("Latitude")
 
-# Separate by species only
-base_plot + facet_grid(~ species)
-
 # Separate by species and period
-base_plot + facet_grid(PERIOD ~ species)
-
+base_tile + facet_grid(PERIOD ~ species)
 
 
 
@@ -108,6 +113,14 @@ ggplot(boot_ruf3_glm, aes(x = WIND_SPEED, y = detected)) +
     xlab(expression("Wind speed (km " * hr^{-1} * ")")) + 
     ylab("Detection of ruffed grouse")
 
+# --------
+# GLM w/ spatial autocorrelation
+# --------
+
+
+
+
+
 
 # --------
 # GLMM
@@ -123,4 +136,18 @@ AIC(ruf3_glmm)
 
 # Diagnostic plots??
 plot(ruf3_glmm)
+
+
+
+
+
+
+
+
+# # Moving this file and CSV to Box folder...
+# system(
+#     paste("cd", getwd(),
+#           "&& cp grouse_data_7Sep16.csv Nell_ps1.R",
+#           "~/'Box Sync/ZooEnt_540_2016/Homework Folders/L_Nell/ps1/'")
+# )
 
