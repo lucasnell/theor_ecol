2ed89866bc23f6d9227865a1d1bdbc6c9633a1ef
lucasnell <lucnell@gmail.com>
2016-09-14 10:46:09 -0500

    Finished PS2

--- Nell_PS2.R
+++ Nell_PS2.R
@@ -1,6 +1,6 @@
 
 # Install required packages if they're not already installed, then load
-for (f in c('dplyr', 'ggplot2', 'tidyr', 'broom')) {
+for (f in c('dplyr', 'ggplot2', 'grid')) {
     if (!f %in% rownames(installed.packages())) {
         install.packages(f, dependencies = TRUE)
     }
@@ -41,102 +41,303 @@ for (f in c('dplyr', 'ggplot2', 'tidyr', 'broom')) {
 # ========================================================================================
 # ========================================================================================
 
-b0 <- 0
-b1 <- 0
-e.sd <- 1
-n <- 10
+
+# ------------
+# Simulating data
+# ------------
+cont_sim <- function(b0 = 0, b1 = 0, e.sd = 1, n = 10, single_ex = FALSE) {
+    X <- rnorm(n = n)
+    Y <- b0 + b1*X + rnorm(n = n, mean = 0, sd = e.sd)
+    if (single_ex) {
+        return(data_frame(x = X, y = Y))
+    }
+    z <- lm(Y ~ X, data = NULL)
+    out_df <- data_frame(
+        b0.est = z$coef[1],
+        b1.est = z$coef[2],
+        resid2.est = mean(z$resid^2),  # "residual sum of squares"
+        sigma.est = mean(z$resid^2)*n/(n-2),  # "estimated variance of the random error"
+        P = summary(z)$coef[2,4]
+    )
+    return(out_df)
+}
+
 
 nsims <- 1000
-output <- data.frame(b0.est=array(0,dim=nsims), b1.est=0, resid2.est=0, sigma.est=0, P=0)
-for(i in 1:nsims){
-    X <- rnorm(n=n)
-    Y <- b0 + b1*X + rnorm(n=n, mean=0, sd=e.sd)
-    z <- lm(Y ~ X)
-    output$b0.est[i] <- z$coef[1]
-    output$b1.est[i] <- z$coef[2]
-    output$resid2.est[i] <- mean(z$resid^2)
-    output$sigma.est[i] <- mean(z$resid^2)*n/(n-2)
-    output$P[i] <- summary(z)$coef[2,4]
-}
-par(mfrow = c(2,1))
 
-plot(Y ~ X, main="Single Example")
-lines(z$fitted.values[order(X)] ~ X[order(X)])
+set.seed(111)
+sim_df <- replicate(nsims, cont_sim(), simplify = FALSE) %>% bind_rows
+sing_df <- cont_sim(single_ex = TRUE)
 
-hist(output$b1.est, main = paste('mean = ', .001*round(1000*mean(output$b1.est)),'   sd=',.001*round(1000*sd(output$b1.est)), sep=''))
 
+lm_p <- ggplot(sing_df, aes(x, y)) +
+    ggtitle("Single example") + 
+    geom_point() +
+    geom_smooth(method = 'lm', se = FALSE)
+
+sim_b1_h <- ggplot(sim_df, aes(b1.est)) + 
+    geom_histogram(bins = 20, fill = 'dodgerblue') +
+    ggtitle(paste('mean =', sprintf("%.3f", mean(sim_df$b1.est)),
+                  ' sd =', sprintf("%.3f", sd(sim_df$b1.est))))
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(lm_p), 
+                ggplotGrob(sim_b1_h), 
+                size = "last"))
+
+
+
+# ------------
 # Two estimates of sigma
-par(mfrow = c(2,1))
+# ------------
 
-hist(output$resid2, main = paste('mean = ', .001*round(1000*mean(output$resid2)),'   sd=',.001*round(1000*sd(output$resid2)), sep=''))
+# Residual sum of squares
+sim_resid2_h <- ggplot(sim_df, aes(resid2.est)) + 
+    geom_histogram(bins = 30, fill = 'red') +
+    geom_vline(aes(xintercept = 1^2), linetype= 3) +
+    ggtitle(paste('mean =', sprintf("%.3f", mean(sim_df$resid2.est)),
+                  ' sd =', sprintf("%.3f", sd(sim_df$resid2.est)))) + 
+    coord_cartesian(xlim = c(0, 4), ylim = c(0, 150))
 
-hist(output$sigma.est, main = paste('mean = ', .001*round(1000*mean(output$sigma.est)),'   sd=',.001*round(1000*sd(output$sigma.est)), sep=''))
+# Sigma: estimated variance of the random error
+sim_sigma_h <- ggplot(sim_df, aes(sigma.est)) + 
+    geom_histogram(bins = 30, fill = 'blue') +
+    geom_vline(aes(xintercept = 1^2), linetype= 3) +
+    ggtitle(paste('mean =', sprintf("%.3f", mean(sim_df$sigma.est)),
+                  ' sd =', sprintf("%.3f", sd(sim_df$sigma.est)))) + 
+    coord_cartesian(xlim = c(0, 4), ylim = c(0, 150))
 
+grid.newpage()
+grid.draw(rbind(ggplotGrob(sim_resid2_h), 
+                ggplotGrob(sim_sigma_h), 
+                size = "last"))
+
+# ------------
+# Bias
+# ------------
+# Using Mean Signed Deviation (MSD)
+
+# In estimators of sigma
+sum((sim_df$resid2.est - 1) / nrow(sim_df))
+sum((sim_df$sigma.est - 1) / nrow(sim_df))
+
+# In b1
+sum((sim_df$b1.est - 0) / nrow(sim_df))
+
+
+# ------------
+# Efficiency
+# ------------
+# Relative
+{sum((sim_df$resid2.est - 1)^2) / nrow(sim_df)} / 
+{sum((sim_df$sigma.est - 1)^2) / nrow(sim_df)}
+
+
+
+# ------------
 # Type I errors
-mean(output$P < 0.05)
+# ------------
+mean(sim_df$P < 0.05)
 
+
+
+# ------------
 # Consistency
-b0 <- 0
-b1 <- 0
-e.sd <- 1
+# ------------
+
 nrange <- c(10, 20, 50, 100, 200, 500, 1000)
 
-nsims <- 1000
-output <- data.frame(n=array(0,dim=nsims*length(nrange)), b0.est=0, b1.est=0, resid2.est=0, sigma.est=0, P=0)
+mult_ns <- data_frame()
+set.seed(222)
+for(n in nrange){
+    output <- replicate(nsims, cont_sim(n = n), simplify = FALSE) %>% 
+        bind_rows %>%
+        mutate(n = as.integer(n))
+    mult_ns <- bind_rows(mult_ns, output)
+}; rm(n, output)
 
-counter <- 0
-for(n in nrange) for(i in 1:nsims){
-    counter <- counter + 1
-    X <- rnorm(n=n)
-    Y <- b0 + b1*X + rnorm(n=n, mean=0, sd=e.sd)
-    z <- lm(Y ~ X)
-    output$n[counter] <- n
-    output$b0.est[counter] <- z$coef[1]
-    output$b1.est[counter] <- z$coef[2]
-    output$resid2.est[counter] <- mean(z$resid^2)
-    output$sigma.est[counter] <- mean(z$resid^2)*n/(n-2)
-    output$P[counter] <- summary(z)$coef[2,4]
-}
-consistency <- aggregate(output$b1.est, by = list(output$n), FUN = sd)
-names(consistency) <- c("n", "b1.sd")
 
-par(mfrow=c(1,1))
-plot(b1.sd ~ n, data=consistency, typ="l")
+consistency <- mult_ns %>% 
+    group_by(n) %>% 
+    summarize(b1.sd = sd(b1.est))
 
+ggplot(consistency, aes(n, b1.sd)) + geom_line()
+
+
+
+# ------------
 # Power
-b0 <- 0
-e.sd <- 1
-b1range <- c(0, .1, .2, .3, .4, .5)
-n <- 100
+# ------------
+
+b1range <- c(0, 0.1, 0.2, 0.3, 0.4, 0.5)
+
+mult_b1 <- data_frame()
+set.seed(333)
+for(b1 in b1range){
+    output <- replicate(nsims, cont_sim(n = 100, b1 = b1), simplify = FALSE) %>% 
+        bind_rows %>%
+        mutate(b1.true = b1)
+    mult_b1 <- bind_rows(mult_b1, output)
+}; rm(b1, output)
+
+mult_b1$rejected <- mult_b1$P < 0.05
+
+powers <- mult_b1 %>% 
+    group_by(b1.true) %>% 
+    summarize(rejected = mean(rejected))
+
+ggplot(powers, aes(b1.true, rejected)) + geom_line()
+
+
+
+
+
+# ==========================================================================
+# ==========================================================================
+
+# Simulate a binary dataset
+
+# ==========================================================================
+# ==========================================================================
+
+inv_logit <- function(x){
+    exp(x)/(1 + exp(x))
+}
+
+
+bin_sim <- function(b0 = 0, b1 = 0, n = 10, single_ex = FALSE) {
+    X <- rnorm(n = n)
+    Y <- rbinom(n = n, size = 1, prob = inv_logit(b0 + b1*X))
+    if (single_ex) {
+        return(data_frame(x = X, y = Y))
+    }
+    z <- lm(Y ~ X, data = NULL)
+    out_df <- data_frame(
+        b0.est = z$coef[1],
+        b1.est = z$coef[2],
+        resid2.est = mean(z$resid^2),  # "residual sum of squares"
+        sigma.est = mean(z$resid^2)*n/(n-2),  # "estimated variance of the random error"
+        P = summary(z)$coef[2,4]
+    )
+    return(out_df)
+}
 
 nsims <- 1000
-output <- data.frame(b1.true=array(0,dim=nsims*length(b1range)), b0.est=0, b1.est=0, resid2.est=0, sigma.est=0, P=0)
 
-counter <- 0
-for(b1 in b1range) for(i in 1:nsims){
-    counter <- counter + 1
-    X <- rnorm(n=n)
-    Y <- b0 + b1*X + rnorm(n=n, mean=0, sd=e.sd)
-    z <- lm(Y ~ X)
-    output$b1.true[counter] <- b1
-    output$b0.est[counter] <- z$coef[1]
-    output$b1.est[counter] <- z$coef[2]
-    output$resid2.est[counter] <- mean(z$resid^2)
-    output$sigma.est[counter] <- mean(z$resid^2)*n/(n-2)
-    output$P[counter] <- summary(z)$coef[2,4]
-}
-output$rejected <- output$P < 0.05
-power <- aggregate(output$rejected, by = list(output$b1.true), FUN = mean)
-names(power) <- c("b1", "rejected")
-
-par(mfrow=c(1,1))
-plot(rejected ~ b1, data=power, typ="l")
+set.seed(111)
+sim_df <- replicate(nsims, bin_sim(), simplify = FALSE) %>% bind_rows
 
 
+# ------------
+# Two estimates of sigma
+# ------------
+
+# Residual sum of squares
+sim_resid2_h <- ggplot(sim_df, aes(resid2.est)) + 
+    geom_histogram(bins = 30, fill = 'red') +
+    # geom_vline(aes(xintercept = 1^2), linetype= 3) +
+    ggtitle(paste('mean =', sprintf("%.3f", mean(sim_df$resid2.est)),
+                  ' sd =', sprintf("%.3f", sd(sim_df$resid2.est)))) + 
+    coord_cartesian(xlim = c(-0.01, 0.325), ylim = c(0, 150))
+
+# Sigma: estimated variance of the random error
+sim_sigma_h <- ggplot(sim_df, aes(sigma.est)) + 
+    geom_histogram(bins = 30, fill = 'blue') +
+    # geom_vline(aes(xintercept = 1^2), linetype= 3) +
+    ggtitle(paste('mean =', sprintf("%.3f", mean(sim_df$sigma.est)),
+                  ' sd =', sprintf("%.3f", sd(sim_df$sigma.est)))) + 
+    coord_cartesian(xlim = c(-0.01, 0.325), ylim = c(0, 150))
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(sim_resid2_h), 
+                ggplotGrob(sim_sigma_h), 
+                size = "last"))
 
 
 
+# ------------
+# Bias
+# ------------
+# Using Mean Signed Deviation (MSD)
 
+# From simple binomial distribution (does this apply here??)
+# np(1-p); p = inv_logit(0) = 0.5
+sigma_known <- 10 * 0.5 * 0.5
+
+# In estimators of sigma
+sum((sim_df$resid2.est - sigma_known) / nrow(sim_df))
+sum((sim_df$sigma.est - sigma_known) / nrow(sim_df))
+
+# In b1
+sum((sim_df$b1.est - 0) / nrow(sim_df))
+
+
+# ------------
+# Efficiency
+# ------------
+# Relative
+{sum((sim_df$resid2.est - sigma_known)^2) / nrow(sim_df)} /
+{sum((sim_df$sigma.est - sigma_known)^2) / nrow(sim_df)}
+
+
+
+# ------------
+# Type I errors
+# ------------
+mean(sim_df$P < 0.05, na.rm = TRUE)
+
+# Not sure why there are any NaNs
+sim_df %>% filter(is.na(P))
+
+
+# ------------
+# Consistency
+# ------------
+
+nrange <- c(10, 20, 50, 100, 200, 500, 1000)
+
+mult_ns <- data_frame()
+set.seed(222)
+for(n in nrange){
+    output <- replicate(nsims, cont_sim(n = n), simplify = FALSE) %>% 
+        bind_rows %>%
+        mutate(n = as.integer(n))
+    mult_ns <- bind_rows(mult_ns, output)
+}; rm(n, output)
+
+
+consistency <- mult_ns %>% 
+    group_by(n) %>% 
+    summarize(b1.sd = sd(b1.est))
+consistency
+
+ggplot(consistency, aes(n, b1.sd)) + geom_line()
+
+
+
+# ------------
+# Power
+# ------------
+
+b1range <- c(0, 0.1, 0.2, 0.3, 0.4, 0.5)
+
+mult_b1 <- data_frame()
+set.seed(333)
+for(b1 in b1range){
+    output <- replicate(nsims, cont_sim(n = 100, b1 = b1), simplify = FALSE) %>% 
+        bind_rows %>%
+        mutate(b1.true = b1)
+    mult_b1 <- bind_rows(mult_b1, output)
+}; rm(b1, output)
+
+mult_b1$rejected <- mult_b1$P < 0.05
+
+powers <- mult_b1 %>% 
+    group_by(b1.true) %>% 
+    summarize(rejected = mean(rejected))
+powers
+
+ggplot(powers, aes(b1.true, rejected)) + geom_line()
 
 
 
@@ -164,8 +365,9 @@ plot(rejected ~ b1, data=power, typ="l")
 
 # # Moving this file to Box folder...
 # system(
-#     paste("cd", getwd(),
+#     paste("mkdir -p ~/'Box Sync/ZooEnt_540_2016/Homework Folders/L_Nell/ps2/' &&",
+#           "cd", getwd(),
 #           "&& cp Nell_PS2.R",
-#           "~/'Box Sync/ZooEnt_540_2016/Homework Folders/L_Nell/ps1/'")
+#           "~/'Box Sync/ZooEnt_540_2016/Homework Folders/L_Nell/ps2/'")
 # )
 
